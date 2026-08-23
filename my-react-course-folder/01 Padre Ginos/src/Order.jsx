0bc5c0b8119b324863f8c8728d9472d010199a5e
RidaAli110 <rida.webdev110@gmail.com>
2026-08-23 22:05:58 +0100

    Added Comments to understand the confusing parts

---
+++ my-react-course-folder/01 Padre Ginos/src/Order.jsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+import Pizza from "./Pizza";
+
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export default function Order() {
+  //  This empty array state will hold the data from the fetched API
+  const [PizzaTypes, SetPizzaTypes] = useState([]);
+
+  // These are the default sates
+  const [PizzaType, SetPizzaType] = useState("Pepperoni");
+  const [PizzaSize, SetPizzaSize] = useState("M");
+  const [loading, SetLoading] = useState(true);
+
+  let price, selectedPizza;
+
+  if (!loading) {
+    selectedPizza = PizzaTypes.find((pizza) => pizza.id);
+  }
+
+  // Fetching from the pizza API
+  async function fetchPizzaTypes() {
+    const res = await fetch("/api/pizzas");
+    const data = await res.json();
+    // This sets the empty array state to the fetched data
+    SetPizzaTypes(data);
+
+    SetLoading(false);
+  }
+
+  useEffect(() => {
+    (fetchPizzaTypes(), []);
+  });
+
+  return (
+    <div className="order">
+      <h2>Create Order</h2>
+      <form action="">
+        <div>
+          <div>
+            <label htmlFor="pizza-type" value={PizzaType}>
+              Pizza Type
+            </label>
+            <select
+              onChange={(e) => SetPizzaType(e.target.value)}
+              name="pizza-type"
+              value={PizzaType}
+            >
+              {PizzaTypes.map((pizza) => (
+                <option key={pizza.id} value={pizza.id}>
+                  {pizza.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="pizza-size">Pizza Size</label>
+            <div>
+              <span>
+                <input
+                  type="radio"
+                  checked={PizzaSize === "S"}
+                  name="pizza-size"
+                  value="S"
+                  id="pizza-s"
+                  onChange={(e) => SetPizzaSize(e.target.value)}
+                />
+                <label htmlFor="pizza-s">Small</label>
+              </span>
+              <span>
+                <input
+                  checked={PizzaSize === "M"}
+                  type="radio"
+                  name="pizza-size"
+                  value="M"
+                  id="pizza-m"
+                  onChange={(e) => SetPizzaSize(e.target.value)}
+                />
+                <label htmlFor="pizza-m">Medium</label>
+              </span>
+              <span>
+                <input
+                  checked={PizzaSize === "L"}
+                  type="radio"
+                  name="pizza-size"
+                  value="L"
+                  id="pizza-l"
+                  onChange={(e) => SetPizzaSize(e.target.value)}
+                />
+                <label htmlFor="pizza-l">Large</label>
+              </span>
+            </div>
+          </div>
+          <button type="submit">Add to Cart</button>
+          <p>$13.37</p>
+        </div>
+      </form>
+      <div className="order-pizza">
+        <Pizza
+          name="Pepperoni"
+          description="Another Pizza"
+          image="/public/pizzas/pepperoni.webp"
+        />
+      </div>
+    </div>
+  );
+}
